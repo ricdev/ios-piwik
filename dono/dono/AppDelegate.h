38f7dc2d8697a845c465953584b7d616215bfc17
ric <ric.dev@gmail.com>
2016-08-31 13:15:34 +0800

    base project

---
+++ dono/dono/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dono
+//
+//  Created by ric on 31/08/2016.
+//  Copyright © 2016 rminfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
